8c34fe647b711e734b47f140bf1c8c717e3ce0a1
Abhith Shaji <abhithshaji@gmail.com>
2023-11-19 08:14:31 +0530

    Kubernetes blockchain netwrok files added

--- fabric-landregistry-network/prerequsite/scripts/createGenesis.sh
+++
@@ -1,6 +0,0 @@
-
-
-export FABRIC_CFG_PATH=${PWD}configtx
-
-
-configtxgen -profile TwoOrgsOrdererGenesis -channelID system-channel -outputBlock ./system-genesis-block/genesis.block
--- fabric-landregistry-network/prerequsite/scripts/updateAnchorPeer.sh
+++
@@ -1,2 +0,0 @@
-CORE_PEER_LOCALMSPID=$1
-peer channel update -o orderer:7050  -c landchannel -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls  --cafile $ORDERER_CA 
